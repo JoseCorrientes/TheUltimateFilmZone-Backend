d513252fe4c75fa8eb90d8341bea0ed8042b5c93
Jose Ernesto Garcia <josernestogarcia609@gmail.com>
2023-09-15 15:24:07 -0300

    20230915-5

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -2,8 +2,8 @@ import "dotenv/config";
 import axios from "axios";
 
 async function categoryMovies(req, res) {
-  const TMDB_TOKEN = process.env.MOVIE_API_TOKEN;
-  const URL = process.env.URL_TMDB;
+  const TMDB_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGE5ZTJjNjQ0Mzk3ZDYxOWZmZDU3ODNlMmNlMmQ0YSIsInN1YiI6IjY0Y2NmMmU0NDNjZDU0MDExYzhkZTkxZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t9tfab1Q0G4PkmrJP4a0R5qUmWa4ArR3u_N2JgF2tQ8'|| process.env.MOVIE_API_TOKEN;
+  const URL = 'https://api.themoviedb.org/3/' || process.env.URL_TMDB;
 
   const options = {
     method: "GET",
